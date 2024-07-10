5ebe7bfe6394e858bea6f2d6c68bc9abec0f8c57
Somanath10may <somdev12000@gmail.com>
2024-07-10 10:44:55 +0530

    manager and mywork

--- Javaregpage.java
+++ Javaregpage.java
@@ -1,6 +1,11 @@
 "first line return by dev A from Hyderabad"
 "second line return by dev b from Banglore"
+<<<<<<< Updated upstream
 
 
 
 "completed my manager work"
+=======
+"iam right now using the git stash command for the other purpose"
+
+>>>>>>> Stashed changes
